ea978462d8d8e1ad204a80004fbf23a517deda7d
BkMahto <bandan.kashish@gmail.com>
2026-04-06 23:08:19 +0530

    Config updated. New image added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.0.127'],
+  allowedDevOrigins: ['192.168.0.127', '192.168.52.139', 'localhost'],
 };
 
 export default nextConfig;
